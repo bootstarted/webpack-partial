4abb259fa5e2fb1433b04b2c4751c87f590deaae
Izaak Schroeder <izaak.schroeder@gmail.com>
2016-01-24 15:22:24 -0800

    Add `inject` function for manipulating `entry`.
    Sometimes you wish to add new scripts to an existing configurations `entry` property; `inject` lets you do just that. Since `partial` replaces `entry`, this function can be used to in conjunction to add elements.

---
+++ test/spec/inject.spec.js
@@ -0,0 +1,21 @@
+import { expect } from 'chai';
+import inject from '../../lib/inject';
+
+describe('inject', () => {
+  it('should handle strings', () => {
+    expect(inject('index.js', [ 'a', 'b' ])).to.have.length(3);
+  });
+
+  it('should handle arrays', () => {
+    expect(inject(['index.js'], [ 'a', 'b' ])).to.have.length(3);
+  });
+
+  it('should handle objects', () => {
+    expect(inject({ a: 'index.js' }, [ 'a', 'b' ]))
+      .to.have.property('a').to.have.length(3);
+  });
+
+  it('should fail otherwise', () => {
+    expect(() => inject(false)).to.throw(TypeError);
+  });
+});
